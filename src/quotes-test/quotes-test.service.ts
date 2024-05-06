627147e29f96f73f70f27929534a7cebd5970b98
CesarRyudi <cesarryudi@gmail.com>
2024-05-06 10:22:14 -0300

    Count and filter
    Pesquisa agora com contagem do total de linhas e retornando resultando sem case sensitive

--- src/quotes-test/quotes-test.service.ts
+++ src/quotes-test/quotes-test.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateQuotesTestDto } from './dto/create-quotes-test.dto';
 import { UpdateQuotesTestDto } from './dto/update-quotes-test.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class QuotesTestService {
@@ -15,21 +16,98 @@ export class QuotesTestService {
     return 'This action adds a new quotesTest';
   }
 
-  findAll(take: string, skip: string, filter?: string) {
+
+async findAll(take: string, skip: string, filter?: string) {
     const takeNumber = parseInt(take);
     const skipNumber = parseInt(skip);
     const page = skipNumber * takeNumber;
 
-    return this.prismaService.quotesCSV.findMany({
+    const query: Prisma.QuotesCSVFindManyArgs = {
       skip: page,
       take: takeNumber,
-      where: {
-        NOME: {
-          contains: filter,
-        },
-      },
-    });
-  }
+      where: {},
+    };
+
+    if (filter) {
+      query.where = {
+        OR: [
+          {
+            NOME: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            Customer: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            PN: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            DESC: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      };
+    }
+
+    const countQueryArgs: Prisma.QuotesCSVCountArgs = {
+      where: {},
+    };
+
+    if (filter) {
+      countQueryArgs.where = {
+        OR: [
+          {
+            NOME: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            Customer: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            PN: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+          {
+            DESC: {
+              contains: filter,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      };
+      // console.log('====> ' + countQueryArgs);
+    }
+ 
+
+    // Query para contar o número total de linhas correspondentes aos critérios de filtragem
+    const countQuery = this.prismaService.quotesCSV.count(countQueryArgs);
+
+    // Query para obter os dados filtrados 
+    const dataQuery = this.prismaService.quotesCSV.findMany(query);
+
+    // Executar ambas as consultas em paralelo
+    const [count, data] = await Promise.all([countQuery, dataQuery]);
+
+    return { count, data };
+}
+
 
   findOne(id: number) {
     return `This action returns a #${id} quotesTest`;
